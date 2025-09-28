e2683ada31301b49869927f2d1fbaa18f330023b
timmyd4 <timmywilliams4665@gmail.com>
2025-09-28 05:27:06 -0700

    saving-work

---
+++ Challenges/Ch2/Ch2App.jsx
@@ -0,0 +1,11 @@
+import Header from "../../Components/Header.jsx";
+import Entry from "../../Components/Entry.jsx";
+export default function App()
+{
+    return (
+        <>
+            <Header/>
+            <Entry/>
+        </>
+);
+}
---
+++ Components/Entry.jsx
@@ -0,0 +1,29 @@
+/**
+ * Challenge: Build out the Entry component and render 1 instance of it
+ * to the App
+ * 
+ * For now, just hard-code in the data, which you can find in
+ * japan.md so you don't have to type it all out manually :)
+ * 
+ * Notes:
+ * – Only render 1 instance of this Entry component for now
+ * – I've pulled in marker.png for the little map marker icon
+ *   that goes next to the location name
+ * – The main purpose of this challenge is to show you where our limitations
+ *   currently are, so don't worry about the fact that you're hard-coding all
+ *   this data into the component.
+ */
+
+export default function Entry()
+{
+    return (
+        <article>
+            <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount-Fuji" />
+            <div>
+                <img src="../images/marker.png" alt="map marker icon" />
+                <span>Japan</span>
+                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
+            </div>
+        </article>
+    )
+}

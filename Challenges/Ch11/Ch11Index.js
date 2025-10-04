72b2d6208891c2994bf88a96b57e8c4dba386681
Tim <timmywilliams4665@gmail.com>
2025-10-04 11:42:14 -0700

    finished js challenge

---
+++ Challenges/Ch11/Ch11Index.js
@@ -0,0 +1,45 @@
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+
+const mapped = nums.map((x) => x ** 2);
+
+console.log(mapped);
+
+
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+const grabbed = names.map((x) => { return x[0].toUpperCase() + x.slice(1)})
+console.log(grabbed);
+
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+
+const element = pokemon.map((x) => {
+    return `<p>${x}</p>`
+})
+
+console.log(element);
